a96bf4a9b5c832a933f1afc5ff857d341d04e62b
David Massegur <65506472+dmassegur@users.noreply.github.com>
2023-03-15 11:15:47 +0000

    Update rainfall_conversion.py
    Adding documentation to `rainfall_conversion.py`  #2

--- rainfall_conversion.py
+++ rainfall_conversion.py
@@ -1,4 +1,16 @@
+''' Functions for converting imperial units to metric.'''
+
 def inches_to_mm(inches):
+    ''' 
+    Converts inches to mm.
+    
+    Arguments:
+        inches: value in inches.
+    
+    Returns:    
+        mm: value in millimetres.
+    '''
+    
     mm = inches * 25.4
     return mm
 
