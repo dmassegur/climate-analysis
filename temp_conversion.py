51b04bf178b6774f6714cd0f99fe2633b4bd1852
David Massegur <65506472+dmassegur@users.noreply.github.com>
2023-03-15 12:53:40 +0000

    Update temp_conversion.py
    Added code documentation

--- temp_conversion.py
+++ temp_conversion.py
@@ -1,10 +1,33 @@
-# Todo: Code is a bit unclear
+"""
+A module for converting temperature from Imperial to Metric units.
+Will throw ValueErrors for temperatures < absolute zero.
 
-def Fc(x):
-    Y = (x - 32) * (5 / 9)
-    return Y
+Functions:
+    convert_fahr_to_celsius: Converts Fahrenheit to Celsius
+    convert_fahr_to_kelvin: Converts Fahrenheit to Kelvin
+"""
 
-def FK(x):
-    y = Fc(x)
-    z = y + 273.15
-    return z
+def convert_fahr_to_celsius(fahr, precision):
+    """
+    Given a temperature in Fahrenheit, converts it to Celsius
+
+    :param fahr: The temperature in Fahrenheit
+    :param precision: number of significant digits of precision.
+    :raises ValueError: If the temperature is below absolute zero
+    :return: The temperature in Celsius
+    """
+
+    assert isinstance(precision, int)
+    
+    temp_celsius = (fahr - 32) * (5 / 9)
+    
+    if temp_celsius < -273.15:
+        # If temperature is below absolute zero, throw an error.
+        raise ValueError(f"Trying to convert impossible temperature: {fahr}F")
+    return temp_celsius
+
+def convert_fahr_to_kelvin(x):
+    temp_kelvin = convert_fahr_to_celsius(x)
+    temp_kelvin += 273.15
+    assert temp_kelvin > 0.0
+    return temp_kelvin
